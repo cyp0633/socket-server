03a505b3e7dd6c2ac85a7bf9cacba976fd93133a
cyp0633 <cyp0633@cyp0633.icu>
2022-11-22 10:25:05 +0800

    Rebuild Client type

--- internal/client.go
+++ internal/client.go
@@ -3,13 +3,14 @@ package internal
 import "net"
 
 type Client struct {
-	Addr string
-	Conn net.Conn
+	Addr     string
+	Conn     net.Conn
+	Messages chan Message
 }
 
-var Clients map[string]net.Conn
+var Clients = []Client{}
 
 func AddClient(conn net.Conn) {
 	addr := conn.RemoteAddr().(*net.TCPAddr).IP.String()
-	Clients[addr] = conn
+	Clients = append(Clients, Client{Addr: addr, Conn: conn, Messages: make(chan Message)})
 }
--- internal/protocol.go
+++ internal/protocol.go
@@ -7,8 +7,8 @@ var Helo = regexp.MustCompile(`^HELO .+`)
 // ProcessHelo 返回在线客户端列表
 func ProcessHelo(msg string) (clients string) {
 	clients = "CLIENTS "
-	for addr := range Clients {
-		clients += addr + " "
+	for _, client := range Clients {
+		clients += client.Addr + " "
 	}
 	return
 }
